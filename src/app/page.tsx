95e71e84d084ad449d3ba64416eafc2512fa434f
harunath <harunath04@gmail.com>
2025-02-28 22:10:59 +0530

    bizz network

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
-		<div>
-			<h1 className="text-3xl">BIZZ NETWORK</h1>
+		<div className="flex items-center justify-center min-h-screen bg-gray-100">
+			<h1 className="text-4xl font-bold text-gray-900">Bizz Network</h1>
 		</div>
 	);
 }
