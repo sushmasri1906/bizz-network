7f7435be592eee66e4738550c3d3157814526e98
sushmasri1906 <sushmasri1906@gmail.com>
2025-03-06 21:44:34 +0530

    Footer updated

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
 
 					<div>
 						<h3 className="text-lg font-semibold text-white mb-2">
-							Quick Links
+							Quick Link
 						</h3>
 						<ul className="space-y-2">
 							<li>
