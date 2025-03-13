586bb1e147c43c355684003665656d3434b7a29d
sushmasri1906 <sushmasri1906@gmail.com>
2025-03-13 11:14:49 +0530

    homepage updated

--- src/components/Home/Homepage.tsx
+++ src/components/Home/Homepage.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Hero from "./Hero";
-// import WhyJoinUs from "./WhyJoinUs";
+import WhyJoinUs from "./WhyJoinUs";
 // import ProfilePage from "../Profile/ProfilePage";
 
 function Homepage() {
 	return (
 		<>
 			<Hero />
-			{/* <WhyJoinUs /> */}
+			<WhyJoinUs />
 			{/* <ProfilePage /> */}
 		</>
 	);
